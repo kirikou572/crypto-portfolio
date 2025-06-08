cd7e9925f942f72ec783f0fe48450cdcbb4f9caa
kirikou572 <55796884+kirikou572@users.noreply.github.com>
2025-06-08 19:40:26 +0200

    Update server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -10,19 +10,19 @@ import authRoutes from './routes/auth.js';
 import portfolioRoutes from './routes/portfolio.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, {
+await mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('✅ MongoDB connecté'))
-  .catch((err) => console.error('❌ Erreur MongoDB', err));
+  useUnifiedTopology: true
+});
+
+const app = express();
 
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/portfolio', portfolioRoutes);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`🚀 Serveur sur http://localhost:${PORT}`);
-});
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Serveur sur http://localhost:${PORT}`));
