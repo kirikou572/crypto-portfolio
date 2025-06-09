00833b9011bad17deb6dcbbdc1307f8b82ddacb7
kirikou572 <55796884+kirikou572@users.noreply.github.com>
2025-06-09 10:38:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/crypto-portfolio/',
   plugins: [react()],
-  server: {
-    port: 5173
-  }
-});
+})
